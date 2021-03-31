2319813429eca81095ee37f77c04003613f3bbeb
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-03-31 06:30:59 +0300

    HttpClient

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
 import { GoalService } from '../goal-service/goal.service';
 import { AlertService } from '../alert-service/alert.service';
+import { Quote } from '../quote-class/quote';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-goal',
@@ -12,6 +14,7 @@ export class GoalComponent implements OnInit {
 
   goals: Goal[]
   alertService: AlertService;
+  quote: Quote;
   
 
   addNewGoal(goal){
@@ -37,12 +40,19 @@ export class GoalComponent implements OnInit {
     }
   }
 
-  constructor(goalService:GoalService, alertService:AlertService) { 
+  constructor(goalService:GoalService, alertService:AlertService, private http: HttpClient) { 
     this.goals = goalService.getGoals()
     this.alertService = alertService;
   }
 
   ngOnInit(): void {
+    interface ApiResponse{
+      author: string;
+      quote: string;
+    }
+    this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+      this.quote = new Quote(data.author, data.quote)
+    })
   }
 
 }
